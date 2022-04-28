de9cbab15635c427803f58a30b03920ce7f50be0
nowadraco@gmail.com <nowadraco@gmail.com>
2022-04-28 05:57:59 -0300

    testes

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -6,8 +6,11 @@ $('.owl-carousel').owlCarousel({
         0:{
             items:1
         },
+        300:{
+            items:2
+        },
         600:{
-            items:3
+            items:4
         },
         1000:{
             items:5
