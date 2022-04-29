eb824f9f3fdc6bfc5cf787fde2261a2e1a973b3d
nowadraco@gmail.com <nowadraco@gmail.com>
2022-04-29 13:49:17 -0300

    updat arquivos

---
+++ js/main.js
@@ -0,0 +1,7 @@
+    function abrir(id){
+      document.getElementById(id).style.display = "block";
+    }
+    
+    function fechar(id){
+      document.getElementById(id).style.display = "none";
+    }
