c4e4e1a0e20faf089fcc88130ab8eacc41126516
nowadraco@gmail.com <nowadraco@gmail.com>
2022-05-10 01:35:11 -0300

    atualizar

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-    function abrir(id){
+    function abrir(id) {
       document.getElementById(id).style.display = "block";
     }
-    
-    function fechar(id){
+
+    function fechar(id) {
       document.getElementById(id).style.display = "none";
     }
+
