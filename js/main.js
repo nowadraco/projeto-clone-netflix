f7baf34701f8c530e9f78ded69380b9521f4e69f
nowadraco <Nowadraco@gmail.com>
2022-06-05 03:58:34 +0000

    configurando texto do popup

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 const button = document.querySelector('.popup-button')
 const popup = document.querySelector('.popup-wrapper')
+const txtp = document.querySelector('.txt-principal')
+const maisp = document.querySelector('.espande-principal')
+const menosp = document.querySelector('.retrair-principal')
 
 button.addEventListener('click', () =>{
  popup.style.display = 'block'
- 
 })
+
+maisp.addEventListener('click', () =>{
+    menosp.style.display = 'block'
+    maisp.style.display = 'none' 
+    txtp.style = 'white-space: inherit'
+   })
+
+   menosp.addEventListener('click', () =>{
+    maisp.style.display = 'block'
+    menosp.style.display = 'none'
+    txtp.style = 'white-space: nowrap'
+   })
+
 
 popup.addEventListener('click',event => {
  const classNameOfClickedElement = event.target.classList[0]
